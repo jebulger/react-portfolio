e7d89f34381f2a19bf36bfa8ef8f697ed718e8e5
Jack <jebulger1003@gmail.com>
2023-08-11 03:12:47 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../src/components/Header';
 import Footer from '../src/components/Footer';
 import About from '../src/components/About';
