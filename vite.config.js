a5b6b3eb2973bbd42ca1544152f52d5ccaaf2bd2
Jack Bulger <127152910+jebulger@users.noreply.github.com>
2023-08-11 03:00:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-portfolio/',
-  build: {
-    outDir: 'build',
-  },
   plugins: [react()],
+  base: '/react-portfolio/',
   server: {
     port: 3000,
     open: true
